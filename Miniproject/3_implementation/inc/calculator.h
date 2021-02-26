f9ff0d69338f80457dddf80b780f6673363eca3b
99003579 <tejaswini.b@ltts.com>
2021-02-26 10:05:12 +0530

    Commit

--- Miniproject/3_implementation/inc/calculator.h
+++ Miniproject/3_implementation/inc/calculator.h
@@ -3,6 +3,8 @@
 
 float convertFahCelsius(float fh);
 float convertCelFahrenheit(float c);
-
+float interest(float P,float R, float T);
+float Profit(float CostPrice, float SellingPrice);
+float Loss(float CostPrice, float SellingPrice);
 
 #endif
