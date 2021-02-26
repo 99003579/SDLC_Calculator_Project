c17790c33a53dfadfa6801b8da3a8531f49213a8
99003575 <78539650+99003575@users.noreply.github.com>
2021-02-26 22:35:54 +0530

    Update main.c

--- Miniproject/3_implementation/src/main.c
+++ Miniproject/3_implementation/src/main.c
@@ -16,24 +16,29 @@ int main()
     printf("2. For Celsius To Fahrenheit\n");
     printf("3. Simple Interest\n");
     printf("4. Profit or loss\n");
+    printf("5. Area of Rectangle\n");
+    printf("6. Area of Square\n");
 
     
     printf("\n\nEnter Your Choice\n");
     scanf("%d",&x);
     switch(x)
       {
-        case 1:  printf("Enter temperature in fahrenheit: ");
-                 scanf("%f", &fahrenheit);
+        case 1:  
+	       printf("Enter temperature in fahrenheit: ");
+               scanf("%f", &fahrenheit);
                     //called function to convert fahrenheit to celsius
-                 celsius = convertFahCelsius(fahrenheit);
-                 printf("%.2f Fahrenheit = %.2f Celsius",fahrenheit,celsius);
-                 break;
-         case 2:  printf("Enter temperature in Celsius: ");
-                  scanf("%f", &celsius);
+               celsius = convertFahCelsius(fahrenheit);
+               printf("%.2f Fahrenheit = %.2f Celsius",fahrenheit,celsius);
+               break;
+		    
+         case 2:
+	       printf("Enter temperature in Celsius: ");
+               scanf("%f", &celsius);
                      //called function to convert celsius to fahrenheit
-                  fahrenheit = convertCelFahrenheit(celsius);
-                printf("%.2f Celsius = %.2f Fahrenheit",celsius, fahrenheit);
-                break;
+               fahrenheit = convertCelFahrenheit(celsius);
+               printf("%.2f Celsius = %.2f Fahrenheit",celsius, fahrenheit);
+               break;
           
           case 3:
                 printf("Enter Principal Amount : ");
@@ -44,21 +49,22 @@ int main()
                 scanf("%f",&Time);
                 Result=interest(Amount,Rate,Time);
                 printf("\nSimple Interest is: %.2f\n",Result);
-                  break;
+                break;
 
-        case 4:
+         case 4:
               	printf("Enter Cost Price:");
                 scanf("%f",&CostPrice);
                 printf("Enter SellingPrice:");
                 scanf("%f",&SellingPrice);
-                  if (SellingPrice == CostPrice)
-                      printf("\nNo profit nor Loss\n");
-                  else if (SellingPrice > CostPrice)
-                      printf("\n%f Profit\n", Profit(CostPrice, SellingPrice));
-                  else
-                      printf("\n%f Loss\n", Loss(CostPrice, SellingPrice));
-                            break;
-        case 5:
+                if (SellingPrice == CostPrice)
+                printf("\nNo profit nor Loss\n");
+                else if (SellingPrice > CostPrice)
+                printf("\n%f Profit\n", Profit(CostPrice, SellingPrice));
+                else
+                printf("\n%f Loss\n", Loss(CostPrice, SellingPrice));
+                break;
+		    
+         case 5:
                 printf("Enter the length : ");
                 scanf("%d", &l);
                 printf("Enter the width : ");
@@ -67,12 +73,12 @@ int main()
                 printf("The area of the rectangle : %d", area);
                 break;
 
-        case 6:
-               printf("enter side of square: ");
-	             scanf("%f",&s);
-	             a=areaofsquare(s);
-            	 printf("AOS: %f\n",a);
-	             break;     
+         case 6:
+                printf("enter side of square: ");
+	        scanf("%f",&s);
+	        a=areaofsquare(s);
+                printf("AOS: %f\n",a);
+	        break;     
    
          
          default:  printf("\n\nInvalid input\n");
